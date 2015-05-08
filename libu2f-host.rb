61ec53425cef1d3a3f27745a47abf77bbed95902
Gert Van Gool <gertvangool@gmail.com>
2015-05-08 10:07:06 +0200

    libu2f-host contains binaries, so adds tests

--- libu2f-host.rb
+++ libu2f-host.rb
@@ -4,8 +4,6 @@ class Libu2fHost < Formula
   version "0.0.4"
   sha256 "852231611bd5c526406b984ae3c92ce3423ffc7a0ef01f6a060a43b64725ead6"
 
-  keg_only "Development library for u2f tools"
-
   depends_on "json-c"
   depends_on "hidapi"
 
@@ -23,15 +21,6 @@ class Libu2fHost < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test libu2f-host`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "u2f-host", "--version"
   end
 end
